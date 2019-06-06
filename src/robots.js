c619c5745e760c2f024e84787cb8e7c470414751
seebar <see.barlow@gmail.com>
2019-06-06 15:07:54 -0700

    First Commit

---
+++ src/robots.js
@@ -0,0 +1,75 @@
+export const robots = [
+
+    {
+        id: 1,
+        name: "Noel Mota",
+        username: "domota",
+        email: "motaa.noel@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Jason Barlow",
+        username: "Jidd",
+        email: "jidd@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Corey Morinaka",
+        username: "Como",
+        email: "Como@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Michael Airhart",
+        username: "Gay",
+        email: "GayGayGay@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Rizmont",
+        username: "Riz",
+        email: "Rizmont@gmail.com"
+    },
+    {
+        id: 6,
+        name: "David Airhart",
+        username: "Dizz",
+        email: "dizz@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Sara Budha",
+        username: "Sizz",
+        email: "Sizzle@gmail.com"
+    },    
+    {
+        id: 8,
+        name: "Vanessa Airhart",
+        username: "VanCar",
+        email: "VanCar@gmail.com"
+    },
+    {
+        id: 9,
+        name: "Jared Airhart",
+        username: "Jazz",
+        email: "JazzyLady@gmail.com"
+    },
+    {
+        id: 10,
+        name: "Shareen Hill",
+        username: "Shady",
+        email: "ShadyShareen@gmail.com"
+    },
+    {
+        id: 11,
+        name: "Uwe",
+        username:"Uwe",
+        email: "Uwe@gmail.com"
+    },
+    {
+        id: 12,
+        name: "Mallory Airhart",
+        username: "Mal",
+        email: "Mal@gmail.com"
+    }
+];
